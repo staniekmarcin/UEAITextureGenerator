fee3aff51513caaae1ea77a405afa5dc8fac16e9
Marcin Staniek <marcin.staniek97@gmail.com>
2025-05-17 12:00:13 +0200

    missing window base class

---
+++ Source/UEAITextureGenerator/Private/UEAITextureGeneratorWindow.cpp
@@ -0,0 +1,19 @@
+// Copyright Your Company. All Rights Reserved.
+
+#include "UEAITextureGeneratorWindow.h"
+#include "Modules/ModuleManager.h"
+
+
+#define LOCTEXT_NAMESPACE "UEAITextureGenerator"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+void SUEAITextureGeneratorWindow::Construct(const FArguments& InArgs)
+{
+    SWindow::Construct(InArgs);
+    
+}
+
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/UEAITextureGenerator/Public/UEAITextureGeneratorWindow.h
@@ -0,0 +1,19 @@
+// Copyright Your Company. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Widgets/SWindow.h"
+
+
+class SUEAITextureGeneratorWindow : public SWindow
+{
+public:
+	virtual void Construct(const FArguments& InArgs);
+
+private:
+	
+};
+
+
